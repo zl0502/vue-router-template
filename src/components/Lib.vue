bffdc8912a3ba6f498abdff44f637b8c2d1196c5
zl0502 <1459816730@qq.com>
2019-03-27 09:37:44 +0800

    vue-router-template project

---
+++ src/components/Lib.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="container">
+		<div class="side-bar">
+			<router-link to="/question-library">题库</router-link>
+			<router-link to="/resource-library">资源库</router-link>
+			<router-link to="/action-library">活动库</router-link>
+		</div>
+		<div class="content"><router-view /></div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Lib',
+	data() {
+		return {
+			
+		};
+	}
+};
+</script>
+
+<style>
+	<style scoped>
+	.container {
+		display: flex;
+		width: 90%;
+		margin: 0 auto;
+		
+	}
+	.side-bar {
+		flex: 1 1 20%;
+		display: flex;
+		flex-direction: column;
+		background-color: #fff;
+		margin-top: 20px;
+	}
+	a{
+		margin-bottom: 30px;
+	}
+	.content{
+		flex: 1 1 80%;
+		text-align: left;
+		background-color: #fff;
+		margin-left: 20px;
+		padding-bottom: 20px;
+	}
+</style>
---
+++ src/components/QuestionLibrary.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h2>账号安全</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'AccountSecurity',
+		data() {
+			return {
+				
+			};
+		}
+	};
+</script>
+
+<style>
+</style>
